bbb3a55fabb7143182c84546409f78db9ab99205
Sumaiya Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2026-07-26 00:13:50 +0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+// import Navbar from "./components/Navbar";
+// import Hero from "./components/Hero";
 
-import About from "./sections/About";
-import Skills from "./sections/Skills";
-import Projects from "./sections/Projects";
-import Contact from "./sections/Contact";
-import Footer from "./sections/Footer";
+// import About from "./sections/About";
+// import Skills from "./sections/Skills";
+// import Projects from "./sections/Projects";
+// import Contact from "./sections/Contact";
+// import Footer from "./sections/Footer";
+
+// function App() {
+//   return (
+//     <main className="bg-[#050816] text-white">
+//       <Navbar />
+//       <Hero />
+//       <About />
+//       <Skills />
+//       <Projects />
+//       <Contact />
+//       <Footer />
+//     </main>
+//   );
+// }
+
+// export default App;
+
+
+import Village from "./scenes/Village";
 
 function App() {
-  return (
-    <main className="bg-[#050816] text-white">
-      <Navbar />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
-    </main>
-  );
+  return <Village />;
 }
 
 export default App;
